269067776a0ee22a057ff809a63fe30588d158a8
Nikita Kovalev <kovalnick434@gmail.com>
2017-03-20 00:52:15 +0300

    First commit (maybe second)

---
+++ Task2/cmake-build-debug/CMakeFiles/Task2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task2.dir/main.cpp.obj"
+  "Task2.pdb"
+  "Task2.exe"
+  "Task2.exe.manifest"
+  "libTask2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Task2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
